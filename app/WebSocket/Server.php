093dca1a548af17147b7d6bf3fb812ee81bff1ac
BetaBond <bond.ryo.beta@gmail.com>
2023-07-19 00:08:34 +0800

    fix: null

--- app/WebSocket/Server.php
+++ app/WebSocket/Server.php
@@ -67,7 +67,7 @@ class Server implements MessageComponentInterface
     {
         $this->command->info("onMessage: $msg");
         foreach ($this->clients as $client) {
-            $this->command->info($from != $client);
+            $this->command->info((string) ($from != $client));
             if ($from != $client) {
                 $data = json_decode($msg, true);
                 
