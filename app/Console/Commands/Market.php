8c78fcf20fd7c9eb8a32f374df66e573336d0f3c
BetaBond <bond.ryo.beta@gmail.com>
2023-07-18 20:02:41 +0800

    fix: null

--- app/Console/Commands/Market.php
+++ app/Console/Commands/Market.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 use function Ratchet\Client\connect;
 
