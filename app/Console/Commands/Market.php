e59d6e631958acd77ca2e895bd0f74e1d098e28f
BetaBond <bond.ryo.beta@gmail.com>
2023-07-18 19:05:09 +0800

    fix: null

--- app/Console/Commands/Market.php
+++ app/Console/Commands/Market.php
@@ -6,6 +6,11 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 use function Ratchet\Client\connect;
 
+/**
+ * 获取火币行情数据
+ *
+ * @author beta
+ */
 class Market extends Command
 {
     
@@ -30,33 +35,71 @@ class Market extends Command
      */
     public function handle(): int
     {
-        connect('wss://api.huobi.pro/ws')->then(function ($conn) {
+        $tokens = ['ethusdt'];
+        $periods = ['1min', '5min', '15min', '30min', '60min', '4hour', '1day'];
+        
+        $message = function ($conn, $msg) {
+            $data = gzdecode($msg);
+            $data = json_decode($data, true);
             
-            // 处理消息
-            $conn->on('message', function (mixed $msg) use ($conn) {
-                $data = gzdecode($msg);
-                $data = json_decode($data, true);
+            // 处理请求
+            if (isset($data['ping'])) {
+                $conn->send(json_encode([
+                    "pong" => $data['ping']
+                ]));
                 
-                // 处理请求
-                if (isset($data['ping'])) {
+                return;
+            }
+            
+            $this->info(json_encode($data));
+        };
+        
+        // 订阅消息
+        $subscribe = function ($conn) use ($tokens, $periods) {
+            
+            foreach ($tokens as $token) {
+                foreach ($periods as $period) {
+                    
+                    // 订阅K线数据
                     $conn->send(json_encode([
-                        "pong" => $data['ping']
+                        "sub" => "market.$token.kline.$period",
+                        "id"  => "sub.market.$token.kline.$period"
                     ]));
+                    
+                    // 一次性拉取订阅
+                    $conn->send(json_encode([
+                        'req' => "market.$token.kline.$period",
+                        "id"  => "req.market.$token.kline.$period"
+                    ]));
+                    
                 }
-                
-                $this->info(json_encode($data));
-                
-                // $conn->close();
+            }
+            
+        };
+        
+        // 异步 websocket 客户端
+        connect(
+            'wss://api.huobi.pro/ws'
+        )->then(function ($conn) use (
+            $subscribe,
+            $message
+        ) {
+            
+            // 接收消息
+            $conn->on('message', function (mixed $msg) use ($conn, $message) {
+                $message($conn, $msg);
             });
             
-            $this->info('发送');
-            $conn->send(json_encode([
-                "sub" => "market.btcusdt.kline.1min",
-                "id"  => "id1"
-            ]));
+            // 关闭消息
+            $conn->on('close', function ($code = null, $reason = null) {
+                $this->warn("CLOSE: 连接已被关闭 ($code - $reason)");
+            });
+            
+            // 订阅消息
+            $subscribe($conn);
+            
         }, function ($e) {
-            $this->info('错误');
-            echo "Could not connect: {$e->getMessage()}\n";
+            $this->error('ERROR: 连接失败 ('.$e->getMessage().')');
         });
         
         return CommandAlias::SUCCESS;
