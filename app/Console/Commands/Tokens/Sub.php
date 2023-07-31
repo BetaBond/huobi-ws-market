4f2b926f90c09adbdfc12bdff7a6a478aaf2808f
BetaBond <bond.ryo.beta@gmail.com>
2023-07-31 21:07:31 +0800

    fix: null

--- app/Console/Commands/Tokens/Sub.php
+++ app/Console/Commands/Tokens/Sub.php
@@ -13,21 +13,21 @@ use Symfony\Component\Console\Command\Command as CommandAlias;
  */
 class Sub extends Command
 {
-    
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'tokens:sub';
-    
+
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = '订阅代币';
-    
+
     /**
      * Execute the console command.
      *
@@ -35,27 +35,30 @@ class Sub extends Command
      */
     public function handle(): int
     {
-        $token = $this->ask('您想要订阅的代币?');
-        
+        $token = $this->ask('您想要订阅的代币(多个以 `,` 分割)?');
+        $tokenSub = explode(',', $token);
+
         $tokens = Cache::get('subscribe.tokens', []);
-        
-        if (in_array($token, $tokens)) {
-            $this->warn('代币已经被订阅!');
-            return CommandAlias::FAILURE;
+
+        foreach ($tokenSub as $value) {
+            if (in_array($value, $tokens)) {
+                $this->warn('代币已经被订阅!');
+                return CommandAlias::FAILURE;
+            }
+
+            $tokens[] = $value;
         }
-        
-        $tokens[] = $token;
-        
+
         $cache = Cache::forever('subscribe.tokens', $tokens);
-        
+
         if (!$cache) {
             $this->warn('订阅失败');
             return CommandAlias::FAILURE;
         }
-        
+
         $this->info('订阅成功');
-        
+
         return CommandAlias::SUCCESS;
     }
-    
+
 }
